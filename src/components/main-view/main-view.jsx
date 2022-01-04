8aa38be3f5bc4de865497cb2abf326960e6d9630
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-01-03 16:35:34 -0800

    added main-view.jsx, movie-card.jsx, and movie-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export default class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+
+    if (movies.length === 0)
+      return <div className="main-view">The list is empty</div>;
+
+    return (
+      <div className="main-view">
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(movie) => {
+                this.setSelectedMovie(movie);
+              }}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
+}
