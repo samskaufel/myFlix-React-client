b1a15135b3bcefd297bee828427a6d1e0803ead9
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-01-10 13:00:59 -0800

    Update registration-view.jsx

--- src/components/registration-view.jsx
+++ src/components/registration-view.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -9,32 +8,30 @@ export function RegistrationView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.onRegister(true, username);
+    props.onRegister(username);
   };
 
    return (
-          <div classname="registration-view">
-              <form className="registration-form">
-              <label>
-              Username:
-              <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </label>
-          <label>
-              Password:
-              <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-          </label>
-          <label>
-              Email:
-              <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-          </label>
-          <label>
-              Birthday:
-              <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-          </label>
-          <button type="submit" onClick={handleSubmit}>Submit</button>
-              </form>
-          </div>
-      )
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+     </form>
+    );
 }
 
 RegistrationView.propTypes = {
