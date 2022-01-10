b4bd5bc31489d3e577441574316ac1245dc51f95
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-01-10 12:20:43 -0800

    updated propTypes in movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -17,9 +17,18 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    Genre: PropTypes.string.isRequired,
-    Director: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string
+  }),
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.string
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
